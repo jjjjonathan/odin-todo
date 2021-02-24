ccd0acba2b7f5e2a3e6fc896d9715c816740fd54
jjjjonathan <hornathanjon@gmail.com>
2021-02-24 00:03:05 -0600

    init dom.js

---
+++ src/dom.js
@@ -0,0 +1,14 @@
+export default (function dom() {
+  const renderProjects = () => {
+    const projectContainer = document.getElementById("project-container");
+  }
+
+  const renderTodos = () => {
+    
+  }
+
+  return {
+    renderProjects,
+    renderTodos,
+  }
+})();
